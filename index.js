aa2ec33bef01ffae41c9f9c0fa966d15c454d1ca
hoanganh2321 <131464214+hoanganh2321@users.noreply.github.com>
2025-02-13 10:27:54 +0700

    Update index.js

--- index.js
+++ index.js
@@ -37,7 +37,7 @@ app.listen(port, () => {
   console.log('\x1b[36m[ SERVER ]\x1b[0m', '\x1b[32m SH : http://localhost:' + port + ' ✅\x1b[0m');
 });
 
-const statusMessages = ["Developer : @vha_kaioshin"];
+const statusMessages = ["Dev : Kazona"];
 const statusTypes = [ 'dnd'];
 let currentStatusIndex = 0;
 let currentTypeIndex = 0;
